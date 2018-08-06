d71ae8a9625e889413ae5580b2182acb2d505853
Joshua Arnold <j.arnold4@uq.edu.au>
2018-08-06 15:20:27 +1000

    weight visualisation working nicely

--- visualiseweights.m
+++ visualiseweights.m
@@ -2,32 +2,65 @@
 %   Assumes 'usual' network variables are already in the work space and
 %   visualises them 
 
-
-% Visualise input connections
-subplot(2, 3, 1);
 neuron_to_plot = 1;
+
+% Visualise network input
+subplot(2, 4, 1);
+locsx = xs(ts > sec * 1000 & ts <= ((sec + 1) * 1000));
+locsy = ys(ts > sec * 1000 & ts <= ((sec + 1) * 1000));
+drawweightedlocs(locsx, locsy, ones(size(locsx)), im_size);
+title(sprintf('Input at %d seconds', sec));
+
+%% Visualise dendrite weights
+subplot(2, 4, 2);
 [locsx, locsy] = ind2sub([im_size, im_size], pre_hid(neuron_to_plot, :));
 drawweightedlocs(locsx, locsy, w_hid(1, :), im_size);
 title(sprintf('Dendrite weights of N%d', neuron_to_plot));
 
-
-% Visualise axon connections
-subplot(2, 3, 2);
+%% Visualise axon weights
+subplot(2, 4, 6);
 [locsx, locsy] = ind2sub([im_size, im_size], post_out(neuron_to_plot, :) - N_inp - N_hid);
 drawweightedlocs(locsx, locsy, w_out(neuron_to_plot, :), im_size);
 title(sprintf('Axon weights of N%d', neuron_to_plot));
 
-% Visualise input variance
-subplot(2, 3, 3);
+%% Visualise general connectivity
+subplot(4, 4, 9);
+[locsx, locsy] = ind2sub([im_size, im_size], pre_hid(neuron_to_plot, :));
+drawlocs(locsx, locsy, im_size);
+title('Dendrite connections');
+
+subplot(4, 4, 13);
+[locsx, locsy] = ind2sub([im_size, im_size], post_out(neuron_to_plot, :) - N_inp - N_hid);
+drawlocs(locsx, locsy, im_size);
+title('Axon connections');
+
+% Visualise dendrite variance
+subplot(2, 4, 8);
+[locsx, locsy] = ind2sub([im_size, im_size], post_out(neuron_to_plot, :) - N_inp - N_hid);
+drawweightedlocs(locsx, locsy, variance_out(neuron_to_plot, :), im_size);
+title(sprintf('Dendrite variance of N%d', neuron_to_plot));
+
+% Visualise axon variance 
+subplot(2, 4, 4);
+[locsx, locsy] = ind2sub([im_size, im_size], pre_hid(neuron_to_plot, :));
+drawweightedlocs(locsx, locsy, variance_hid(neuron_to_plot, :), im_size);
+title(sprintf('Dendrite variance of N%d', neuron_to_plot));
 
 
-% Visualise output delays
+%% Visualise dendrite delays
+subplot(2, 4, 3);
+[locsx, locsy] = ind2sub([im_size, im_size], pre_hid(neuron_to_plot, :));
+drawweightedlocs(locsx, locsy, delays_hid(neuron_to_plot, :), im_size);
+title(sprintf('Dendrite delays of N%d', neuron_to_plot));
 
-% Visualise 
+%% Visualise axon delays
+subplot(2, 4, 7);
+[locsx, locsy] = ind2sub([im_size, im_size], post_out(neuron_to_plot, :) - N_inp - N_hid);
+drawweightedlocs(locsx, locsy, delays_out(neuron_to_plot, :), im_size);
+title(sprintf('Axon delays of N%d', neuron_to_plot));
 
 
-
-
+%% helper functions
 function [] = drawweightedlocs( locsx, locsy, weightings, im_size )
 
     pts = linspace(1, im_size + 1, im_size + 1) - 0.5;
